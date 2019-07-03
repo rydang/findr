1240759ccba38bfabb5c5c933dc4ca381d2cd749
Ryan Dang <rydang@umich.edu>
2019-07-03 09:35:39 -0400

    database can accept post requests for new friends and interests

--- server/database.js
+++ server/database.js
@@ -16,36 +16,40 @@ pool.connect()
     const friendsCreation = db.query(
       `CREATE TABLE IF NOT EXISTS "friends" (
         id SERIAL PRIMARY KEY,
-        first_name VARCHAR(45),
-        last_name VARCHAR(45)
+        first_name VARCHAR(45) NOT NULL,
+        last_name VARCHAR(45) NOT NULL,
+        phone_number VARCHAR(12) NOT NULL,
+        username VARCHAR(100) NOT NULL,
+        password VARCHAR(100) NOT NULL
       )`,
     );
     const interestsCreation = db.query(
       `CREATE TABLE IF NOT EXISTS "interests" (
         id SERIAL PRIMARY KEY,
         friend_id smallint NOT NULL,
-        interest VARCHAR(45)
+        interest VARCHAR(45) NOT NULL
       )`,
     );
-    Promise.all([friendsCreation, interestsCreation]);
+    return Promise.all([friendsCreation, interestsCreation]);
   })
+  // add default entries to tables
   .then(() => {
     // if friends table is empty, then add default users
     db.query('SELECT * FROM friends')
       .then(({ rows }) => {
         if (!rows.length) {
           const users = [
-            'Brian', 'Wang',
-            'Bryan', 'Yang',
-            'Lion', 'Tame',
-            'Lyon', 'Tayme',
-            'Rian', 'Lang',
+            'Brian', 'Wang', '111-111-1111', 'brianwang', 'hunter2',
+            'Bryan', 'Yang', '222-222-2222', 'bryanyang', 'hunter2',
+            'Lion', 'Tame', '333-333-3333', 'liontame', 'hunter2',
+            'Lyon', 'Tayme', '444-444-4444', 'lyontayme', 'hunter2',
+            'Rian', 'Lang', '555-555-5555', 'rianlang', 'hunter2',
           ];
 
-          let insertQuery = 'INSERT INTO friends(first_name, last_name) VALUES';
-          for (let i = 0; i < users.length; i += 2) {
-            insertQuery += `($${i + 1}, $${i + 2})`;
-            if (i < users.length - 2) {
+          let insertQuery = 'INSERT INTO friends(first_name, last_name, phone_number, username, password) VALUES';
+          for (let i = 0; i < users.length; i += 5) {
+            insertQuery += `($${i + 1}, $${i + 2}, $${i + 3}, $${i + 4}, $${i + 5})`;
+            if (i < users.length - 5) {
               insertQuery += ',';
             }
           }
@@ -93,9 +97,38 @@ pool.connect()
 
 pool.end();
 
-router.get('/', (req, res) => {
-  db.query('SELECT * FROM friends, interests WHERE friends.id=interests.friend_id')
-    .then(friends => res.status(200).send(friends.rows))
+router.get('/', (req, res) => db.query('SELECT * FROM friends, interests WHERE friends.id=interests.friend_id')
+  .then(friends => res.status(200).send(friends.rows))
+  .catch(err => res.status(400).send(err)));
+
+router.post('/friend', (req, res) => {
+  const {
+    firstName,
+    lastName,
+    phoneNumber,
+    username,
+    password,
+  } = req.body;
+  const parameters = [firstName, lastName, phoneNumber, username, password];
+
+  db.query('INSERT INTO "friends" (first_name, last_name, phone_number, username, password) VALUES ($1,$2,$3,$4,$5)', parameters)
+    .then(() => res.status(200).send('success'))
+    .catch(err => res.status(400).send(err));
+});
+
+router.post('/interest', (req, res) => {
+  const { interests } = req.body;
+
+  let insertQuery = 'INSERT INTO "interests" (friend_id, interest) VALUES';
+  const parameters = [];
+  interests.forEach((entry, i) => {
+    insertQuery += `($${2 * i + 1},$${2 * i + 2})`;
+    parameters.push(entry.friend_id, entry.interest);
+    if (2 * i + 1 < interests.length) insertQuery += ',';
+  });
+
+  db.query(insertQuery, parameters)
+    .then(() => res.status(200).send('success'))
     .catch(err => res.status(400).send(err));
 });
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const database = require('./database');
 
 const app = express();
 
+app.use('/build', express.static('build'));
+
+app.use(bodyParser.json());
+
 app.use('/api', database);
 
 if (process.env.NODE_ENV !== 'development') {
-  console.log('in production');
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/index.html'));
   });
